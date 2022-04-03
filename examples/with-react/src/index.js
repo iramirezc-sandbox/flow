83f477d62997b8d2def3de65e1d58afc427a52be
Isaac Ramirez <iramirezc@live.com.mx>
2022-04-02 23:13:10 -0600

    Note to index file explaining the reason of not adding flow.

--- examples/with-react/src/index.js
+++ examples/with-react/src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+// TODO: Flow complains with "Cannot resolve module `react-dom/client`".
+// Installing flow-typed does not help either, as there are no
+// type definitions for version 18 yet.
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
