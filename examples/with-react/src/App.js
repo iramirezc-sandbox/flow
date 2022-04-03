bf0fc657529e7576c1653c865947e5b6dfc49d36
Isaac Ramirez <iramirezc@live.com.mx>
2022-04-02 23:26:10 -0600

    Move styles to a single object.

--- examples/with-react/src/App.js
+++ examples/with-react/src/App.js
@@ -2,20 +2,19 @@
 import React from "react";
 import { square } from "./utils";
 
+const styles = {
+  container: { display: "flex", flexDirection: "column", alignItems: "center" },
+  title: {
+    color: "#61dbfb",
+    backgroundColor: "#242424",
+    padding: "0 15px",
+  },
+};
+
 const App = (): React$Element<"div"> => (
-  <div
-    style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-  >
+  <div style={styles.container}>
     <h1>
-      <span
-        style={{
-          color: "#61dbfb",
-          backgroundColor: "#242424",
-          padding: "0 15px",
-        }}
-      >
-        React + Flow
-      </span>
+      <span style={styles.title}>React + Flow</span>
     </h1>
     <pre>
       <code>square(2) = {square(2) * 1}</code>
